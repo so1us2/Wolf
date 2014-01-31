7e748b7c3e1e4c0bf471e5125150a991c9d68a3e
Tom Martell <tmartell@gmail.com>
2014-01-31 01:22:32 -0800

    added additional setup actions.

---
+++ src/wolf/action/setup/ExplainAction.java
@@ -0,0 +1,52 @@
+package wolf.action.setup;
+
+import java.util.List;
+import java.util.Map;
+
+import wolf.model.Player;
+import wolf.model.SetupStage;
+
+import com.google.common.collect.Maps;
+
+public class ExplainAction extends SetupAction {
+
+  static final Map<String, String> topics = Maps.newLinkedHashMap();
+
+  static {
+    topics.put("Seer",
+        "The Seer is a villager who can find out the affiliation of one player each night.");
+    topics
+        .put(
+            "Medic",
+            "The Medic is a villager who can protect one person from being killed each night. The medic may not protect the same person twice in a row.");
+
+    topics.put("Villager",
+        "The villagers are trying to find and kill the wolves before they get eaten.");
+    topics
+        .put("Wolf",
+            "The wolves are trying to hide their identities by day and kill off the villagers at night.");
+  }
+
+  public ExplainAction(SetupStage stage) {
+    super(stage, "explain", 1);
+  }
+
+  @Override
+  protected void execute(Player invoker, List<String> args) {
+    String topic = args.get(0);
+
+    if(!topics.containsKey(topic)) {
+      StringBuilder output = new StringBuilder();
+      output.append("Unsupported Topic. Please try: ").append(topics.keySet());
+      getBot().sendMessage(output.toString());
+    } else {
+      getBot().sendMessage(topic + ": " + topics.get(topic));
+    }
+  }
+
+  @Override
+  public String getDescription() {
+    return "Get an explanation for different game parameters.";
+  }
+
+}
---
+++ src/wolf/action/setup/ListRolesAction.java
@@ -0,0 +1,40 @@
+package wolf.action.setup;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import wolf.model.Player;
+import wolf.model.Role;
+import wolf.model.SetupStage;
+
+public class ListRolesAction extends SetupAction {
+
+  public ListRolesAction(SetupStage stage) {
+    super(stage, "roles", 0);
+  }
+
+  @Override
+  protected void execute(Player invoker, List<String> args) {
+
+    Map<Role, Integer> roles = getStage().getRoles();
+
+    if (roles.isEmpty()) {
+      getBot().sendMessage("No roles have been setup yet.");
+    } else {
+      StringBuilder output = new StringBuilder();
+      output.append("Current roles: ");
+      for (Entry<Role, Integer> e : roles.entrySet()) {
+        output.append(e.getKey()).append(" (").append(e.getValue()).append("), ");
+      }
+      output.setLength(output.length() - 2);
+      getBot().sendMessage(output.toString());
+    }
+  }
+
+  @Override
+  public String getDescription() {
+    return "List roles currently loaded in the game.";
+  }
+
+}

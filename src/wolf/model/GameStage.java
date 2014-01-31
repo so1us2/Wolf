b53199eb40d1c1ff3aa4a11d57d19d1c1edc1091
Tom Martell <tmartell@gmail.com>
2014-01-31 13:15:44 -0800

    cleaning up the branch.

--- src/wolf/model/GameStage.java
+++ src/wolf/model/GameStage.java
@@ -18,27 +18,4 @@ public class GameStage extends Stage {
     return ImmutableList.of();
   }
 
-  // private void assignRoles(Map<Class<? extends Role>, Integer> roleCountMap) throws Exception {
-  // List<Player> playersWhoNeedRoles = Lists.newArrayList(namePlayerMap.values());
-  // outerLoop: for (Entry<Class<? extends Game>, Integer> entry : roleCountMap.entrySet()) {
-  // for (int i = 0; i < entry.getValue(); i++) {
-  // if (playersWhoNeedRoles.isEmpty()) {
-  // break outerLoop;
-  // }
-  // Player randomPlayer =
-  // playersWhoNeedRoles.remove((int) (Math.random() * playersWhoNeedRoles.size()));
-  // Role role = entry.getKey().newInstance();
-  // randomPlayer.setRole(role);
-  // }
-  // }
-  //
-  // bot.sendMessage("Assigning roles...");
-  //
-  // for (Player player : namePlayerMap.values()) {
-  // bot.sendMessage(player, "You are a " + player.getRole() + ".");
-  // }
-  //
-  // bot.sendMessage("Roles have been assigned.");
-  // }
-
 }
